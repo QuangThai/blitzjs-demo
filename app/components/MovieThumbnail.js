14a05c145ae2f2d9b28155aa9bd066384225bbbf
Quang Thái <nguyenquangthaib1@gmail.com>
2022-01-22 11:59:52 +0700

    update detail

--- app/components/MovieThumbnail.js
+++ app/components/MovieThumbnail.js
@@ -2,12 +2,12 @@ import { Image, useRouter } from "blitz"
 
 function MovieThumbnail({ result }) {
   const BASE_URL = "https://image.tmdb.org/t/p/original/"
-  // const router = useRouter()
+  const router = useRouter()
 
   return (
     <div
       className="flex min-w-[250px] min-h-[170px] md:min-w-[330px] md:min-h-[210px] rounded-lg overflow-hidden shadow-xl cursor-pointer border-[3px] border-[#f9f9f9] border-opacity-10  hover:border-opacity-80 hover:shadow-2xl transform hover:scale-105 transition duration-300"
-      // onClick={() => router.push(`/movie/${result.id}`)}
+      onClick={() => router.push(`/movie/${result.id}`)}
     >
       <Image
         src={
